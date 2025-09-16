30586dd585d6ff853ca47500136161bac8fd16a3
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2025-09-16 20:01:16 +0200

    added some code to change the background color of the screen

--- kompton_invasion.py
+++ kompton_invasion.py
@@ -9,6 +9,9 @@ class KomptonInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Kompton Invasion")
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+    
 
     def run_game(game):
         """Start the main loop for the game."""
@@ -17,6 +20,9 @@ class KomptonInvasion:
             for event in  pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
